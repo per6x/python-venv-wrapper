9babd8c5a42ef6edebe61c455d517b964509d016
Petr Persianov <persianov.petr@gmail.com>
2022-03-10 13:10:36 +0100

    Init

---
+++ workonvenv.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+function workon() {  
+  venv_name=$1
+  source $VENVS_DIR/$venv_name/bin/activate
+}
+
+function mkvenv() {
+  venv_name=$1
+  venv_dir=$2
+  python_v=$3
+
+  if [ -z "$venv_dir" ] 
+  then
+    venv_dir=$VENVS_DIR
+  fi
+
+  if [ -z "$python_v" ] 
+  then
+    python_v="/usr/bin/python3"
+  fi
+   
+  $python_v -m venv $venv_dir/$venv_name
+}
+
+function list_venvs() {
+  venv_dir=$1
+
+  if [ -z "$venv_dir" ] 
+  then
+    venv_dir=$VENVS_DIR
+  fi
+  for i in $(ls $venv_dir); do echo ${i%%/}; done
+}
+
+  
